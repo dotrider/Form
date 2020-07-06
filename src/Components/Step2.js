b0599e7c5b77ba53e28ed0a2762971d36b859273
dotrider <56666461+dotrider@users.noreply.github.com>
2020-07-06 00:38:57 -0700

    adding MUI to step2

--- src/Components/Step2.js
+++ src/Components/Step2.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import {TextField, AppBar, Toolbar, Typography, Button} from '@material-ui/core'
+
 
 const Step2 = () => {
     return (
-        <div>
-            step2
-        </div>
+        <>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" >
+                    Enter Details
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <TextField
+            label="First Name"
+            
+        />
+        <br/>
+         <TextField
+            label="Last Name"
+    
+        />
+        <br/>
+         <TextField
+            label="Email"
+
+            htmlFor="outlined-adornment-email"
+        />
+        <br/>
+        <Button variant="contained" color="primary"
+  
+        >
+            Back
+        </Button>
+
+        <Button variant="contained" color="primary"
+  
+        >
+            Continue
+        </Button>
+    </>
     )
 }
 
